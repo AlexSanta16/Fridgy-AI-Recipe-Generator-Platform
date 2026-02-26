d6ea926272c23f6c4519269c406ad1ebdb65b195
Alexandru Santamarian <alexsantamarian16@gmail.com>
2026-02-26 09:01:03 +0200

    Update AdminStatsController.java

--- backend/src/main/java/com/project/Fridgy/controller/AdminStatsController.java
+++ backend/src/main/java/com/project/Fridgy/controller/AdminStatsController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "http://localhost:3000")
 public class AdminStatsController {
 
-    @Autowired
+    
     private final AdminStatsService statsService;
 
     public AdminStatsController(AdminStatsService statsService) {
